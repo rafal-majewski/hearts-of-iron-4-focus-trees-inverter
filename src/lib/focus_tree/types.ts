c5b4737072a0bbfd00e6ab603f62540a34fdf5d8
Rafał Majewski <goodheropl@gmail.com>
2023-05-03 22:22:00 +0200

    Focus tree separate file for types

---
+++ src/lib/focus_tree/types.ts
@@ -0,0 +1,24 @@
+export type FocusPosition = {
+	x: number;
+	y: number;
+	relativeToFocusId: string | null;
+};
+
+export type Focus = {
+	id: string;
+	position: FocusPosition;
+	prerequiredFocusIds: {
+		allOf: {
+			anyOf: string[];
+		}[];
+	};
+
+	additionalProperties: Record<string, unknown>;
+};
+
+export type FocusTree = {
+	id: string;
+	focuses: Focus[];
+
+	additionalProperties: Record<string, unknown>;
+};
